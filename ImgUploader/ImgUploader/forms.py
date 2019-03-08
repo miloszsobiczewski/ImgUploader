69f62e9ff79755a15e47701de8d54ac048ed7e10
Milosz Sobiczewski <sobiczewsk.milosz@gmail.com>
2019-03-08 10:08:15 +0100

    models, forms added

---
+++ ImgUploader/ImgUploader/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from . import models
+
+
+class ImageForm(forms.ModelForm):
+    type = forms.ChoiceField(
+        choices=[(x, x) for x in ['local', 'Google Drive']], required=True)
+    size = forms.ChoiceField(
+        choices=[(x, x) for x in ['800x600', '480x640']], required=True)
+
+    class Meta:
+        model = models.Image
+        fields = ['type', 'size', 'picture']
+
+
+
---
+++ ImgUploader/ImgUploader/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Image(models.Model):
+
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=13)
+    size = models.CharField(max_length=9)
+    slug = models.CharField(max_length=100)
+    picture = models.ImageField(default='image.svg', blank=True)
+
+    def __str__(self):
+        return self.name.__str__() + "-" + self.size.__str__()
